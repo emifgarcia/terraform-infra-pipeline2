3c8df0b0bcd5b08e8f081a2cec6c4833bfaa6cdb
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2025-03-22 21:05:13 -0300

    update module source path

--- environments/develop/main.tf
+++ environments/develop/main.tf
@@ -1,13 +1,20 @@
 module "rg-dev-001" {
-  source      = "/Users/marcelofigueiredogarcia/Desktop/terraform-infra-pipeline2/modules/resource_group"
+  source      = "../../modules/resource_group"
   rg_name     = "rg-dev-001"
   rg_location = var.rg_location
 
 }
 
 module "rg-dev-002" {
-  source      = "/Users/marcelofigueiredogarcia/Desktop/terraform-infra-pipeline2/modules/resource_group"
+  source      = "../../modules/resource_group"
   rg_name     = "rg-dev-002"
+  rg_location = var.rg_location
+
+}
+
+module "rg-dev-003" {
+  source      = "../../modules/resource_group"
+  rg_name     = "rg-dev-003"
   rg_location = var.rg_location
 
 }
